f944eaff8a170c4d8fbcdd68ad182875f92ec8da
Med ben Malek <mohamed.benmalekcherif@esprit.tn>
2019-09-24 00:00:38 +0100

    Updates

---
+++ precache-manifest.8692ffd9fe727a7007b7c55f4aa5c48a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50c92bb6e9e3398554b176aca0ad88a",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "a7d8ea39db6e7fadd7b4",
+    "url": "/resume/static/css/2.aa46e993.chunk.css"
+  },
+  {
+    "revision": "aee8fb9df3ad5aba23a8",
+    "url": "/resume/static/css/main.b67c490c.chunk.css"
+  },
+  {
+    "revision": "a7d8ea39db6e7fadd7b4",
+    "url": "/resume/static/js/2.e261a461.chunk.js"
+  },
+  {
+    "revision": "aee8fb9df3ad5aba23a8",
+    "url": "/resume/static/js/main.6022c353.chunk.js"
+  },
+  {
+    "revision": "17cc7308dea17913c7b4",
+    "url": "/resume/static/js/runtime~main.deee99b2.js"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/resume/static/media/ionicons790f.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/resume/static/media/ionicons790f.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/resume/static/media/ionicons790f.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/resume/static/media/ionicons790f.621bd386.svg"
+  }
+]);
